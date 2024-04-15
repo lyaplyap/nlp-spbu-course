de41df8954f6cf4b01e22dbb503913d00c5287f2
Nikita Liapin <lyapinne@gmail.com>
2024-04-15 10:02:17 +0300

    feat(task2): added solution

--- task2/gpt.py
+++ task2/gpt.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import os
+import matplotlib.pyplot as plt
 
 # hyperparameters
 batch_size = 64     # how many independent sequences will we process in parallel?
@@ -16,10 +18,14 @@ n_layer = 6
 dropout = 0.2
 # ------------
 
-torch.manual_seed(1337)
+torch.manual_seed(1234)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+dirname = os.path.dirname(__file__)
+data_path = os.path.join(dirname, 'data', 'the_twelve_chairs.txt')
+dist_path = os.path.join(dirname, 'dist')
+
+# wget https://raw.githubusercontent.com/lyaplyap/nlp-spbu-corse/main/task2/data/the_twelve_chairs.txt
+with open(data_path, 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -48,7 +54,7 @@ def get_batch(split):
     return x, y
 
 @torch.no_grad()
-def estimate_loss():
+def estimate_loss(model):
     out = {}
     model.eval()
     for split in ['train', 'val']:
@@ -136,7 +142,6 @@ class Block(nn.Module):
         return x
 
 class GPTLanguageModel(nn.Module):
-
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
@@ -184,7 +189,7 @@ class GPTLanguageModel(nn.Module):
             # crop idx to the last block_size tokens
             idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx_cond)
+            logits, _ = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :]           # becomes (B, C)
             # apply softmax to get probabilities
@@ -195,31 +200,163 @@ class GPTLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)             # (B, T+1)
         return idx
 
-model = GPTLanguageModel()
-m = model.to(device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+class FunnelBlock(nn.Module):
+    """Transformer block with sequence reduction for a Funnel Transformer"""
 
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    def __init__(self, n_embd, n_head, seq_reduction=False, reduction_factor=2):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.seq_reduction = seq_reduction
+        self.reduction_factor = reduction_factor
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+        
+        if self.seq_reduction:
+            self.pool = nn.AvgPool1d(kernel_size=reduction_factor, stride=reduction_factor)
+        
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        
+        if self.seq_reduction:
+            x_permuted = x.permute(0, 2, 1)
+            x_pooled = self.pool(x_permuted)
+            x = x_pooled.permute(0, 2, 1)
+        
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
-for iter in range(max_iters):
+class FunnelTransformerLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[FunnelBlock(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        self.apply(self._init_weights)
 
-    # sample a batch of data
-    xb, yb = get_batch('train')
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
 
-# generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = tok_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
+            logits, loss = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        
+        return idx
+
+def main(model, path):
+    m = model.to(device)
+    # print the number of parameters in the model
+    print(sum(p.numel() for p in m.parameters()) / 1e6, 'M parameters')
+
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    # create losses list
+    losses_list = []
+
+    for iter in range(max_iters):
+
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0 or iter == max_iters - 1:
+            losses = estimate_loss(model)
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        # sample a batch of data
+        xb, yb = get_batch('train')
+
+        # evaluate the loss
+        _, loss = model(xb, yb)
+        losses_list.append(loss.item())
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    # calculate the exponent of losses
+    losses_exp = torch.exp(torch.tensor(losses_list)).tolist()
+
+    def save_losses(losses_exp, path):
+        with open(os.path.join(path, 'losses_exp.txt'), 'w') as f:
+            for line in losses_exp:
+                f.write(f"{line}\n")
+
+    def draw_chart(losses_exp, path):
+        plt.plot(losses_exp)
+        
+        plt.xlabel('iter')
+        plt.ylabel('exponent of loss')
+        
+        plt.savefig(os.path.join(path, 'losses_exp.png'))
+
+    def generate_context(path):
+        context = torch.zeros((1, 1), dtype=torch.long, device=device)
+        
+        print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+        open(os.path.join(path, 'more.txt'), 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+
+    save_losses(losses_exp, path)
+    draw_chart(losses_exp, path)
+    generate_context(path)
+
+def draw_perplexities_chart():
+    GPT_LAST_LOSS = 1.9325309991836548
+    FT_LAST_LOSS = 1.9285459518432617
+
+    labels = ['Default Model', 'Funnel Transformer']
+    values = [GPT_LAST_LOSS, FT_LAST_LOSS]
+
+    plt.bar(labels, values)
+    plt.title('Perplexities')
+
+    plt.xlabel('models')
+    plt.ylabel('exponent of loss')
+    
+    plt.savefig(os.path.join(dirname, 'perplexity.png'))
+
+# Default Model
+gpt_model = GPTLanguageModel()
+gpt_path = os.path.join(dist_path, 'default_model')
+
+main(gpt_model, gpt_path)
+
+# Funnel Transformer
+ft_model = FunnelTransformerLanguageModel()
+ft_path = os.path.join(dist_path, 'funnel_transformer')
+
+main(ft_model, ft_path)
+
+# Perplexities chart
+draw_perplexities_chart()
